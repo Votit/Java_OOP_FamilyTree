ed3ccb525be991bdff71874412ca9eba0532707c
Ivan_Titov <titovid91@gmail.com>
2024-07-15 23:56:35 +0300

    start

--- Human.java
+++ Human.java
@@ -1,3 +1,22 @@
-public class Human {
+import java.time.LocalDate;
+import java.util.List;
 
+public class Human {
+    static int count;
+
+    private int id;
+    private String name;
+    private Gender gender;
+    private LocalDate dateOfBirth, dateOfDeath;
+    private List<Human> parents;
+    private List<Human> children;
+    private Human spouse;
+
+    Human(String name) {
+        id = count;
+        count++;
+        this.name = name;
+        // this.gender = gender;
+        // this.dateOfBirth = dateOfBirth;
+    }
 }
